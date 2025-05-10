dd79f10699a52520b317d5530ad070a0bbd2ff27
SS-Jane <superjane.prodev@gmail.com>
2025-05-10 08:07:39 +0700

    add Modal detailModal

--- admin/api/controller/BillSaleController.js
+++ admin/api/controller/BillSaleController.js
@@ -1,14 +1,20 @@
 const { PrismaClient } = require("@prisma/client");
+const dayjs = require("dayjs");
+const { end } = require("pdfkit");
 const prisma = new PrismaClient();
 
 module.exports = {
   list: async (req, res) => {
     try {
+
+      const startDate = dayjs(req.body.startDate).set('hour', 0).set('minute',0).set('second',0).toDate();
+      const endDate = dayjs(req.body.endDate).set('hour', 23).set('minute', 59).set('second', 59).toDate();
+
       const billSale = await prisma.billSale.findMany({
         where: {
           createdDate: {
-            gte: req.body.startDate,
-            lte: req.body.endDate,
+            gte: startDate,
+            lte: endDate,
           },
           status: "use",
         },
